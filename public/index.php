d70cbb26b900e08ba33c89e21ed6e57efe17cec6
ibrah91 <ibrahima.traore@live.fr>
2019-03-25 17:03:28 +0100

    routage

--- public/index.php
+++ public/index.php
@@ -7,21 +7,49 @@ use Slim\App;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-//Création de l'application
-
 $app = new App();
 
-//Crétion d'une route
+//Création d'une route
 
-$app->get("/hello", function (
-    ServerRequestInterface $request,
-    ResponseInterface $response,
+$app->get("/", function (
+    ServerRequestInterface $request, ResponseInterface $response,
     ?array $args
 ) {
     //On retourne une réponse
 
     return $response->getBody()->write('<h1>Bonjour ibrah !!! </h1>');
+}
+)
+    ->setName("homepage");
+
+$app->group('/projet', function () {
+
+    $this->get("/{id:\d+}", function (ServerRequestInterface $request, ResponseInterface $response, ?array $args
+
+    ) {
+        //On retourne une réponse
+
+        return $response->getBody()->write('<h1>ah ah ah  !!! </h1>');
+
+    })->setName('app_project_show');
+
+
+    $this->get("/creation", function (ServerRequestInterface $request, ResponseInterface $response, ?array $args
+
+    ) {
+        //On retourne une réponse
+
+        return $response->getBody()->write('<h1>Cretion d\'un projet </h1>');
+
+    })->setName('app_project_create');
 });
+
+// Création d'une page de détail des projets
+// Nouvveauté : on ajoute une variable dans l'URL avec les accolades
+
+//Création de l'application
+
+//Crétion d'une route
 
 //Création et renvoi de la réponse au navigateur
 $app->run();
