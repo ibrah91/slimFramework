2a147b0497ff76a9bd2484eb584f2278a55b0222
ibrah91 <ibrahima.traore@live.fr>
2019-03-25 14:54:06 +0100

    version avec code sniffeur + htaccess

--- public/index.php
+++ public/index.php
@@ -1,5 +1,27 @@
 <?php
+//Autoloader
 
-echo " bonjour ibrah !!! ";
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
 
-?>
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+//Création de l'application
+
+$app = new App();
+
+//Crétion d'une route
+
+$app->get("/hello", function (
+    ServerRequestInterface $request,
+    ResponseInterface $response,
+    ?array $args
+) {
+    //On retourne une réponse
+
+    return $response->getBody()->write('<h1>Bonjour ibrah !!! </h1>');
+});
+
+//Création et renvoi de la réponse au navigateur
+$app->run();
